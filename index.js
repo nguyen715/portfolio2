bdcf225df4c052e86ffe9f1513ae7da1d91d7941
Matthew Rougely <matthewrougelysr@gmail.com>
2020-10-03 18:03:17 -0700

    added menu

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+function slideOut(x) {
+  x.classList.toggle("sliding");
+}
+
+function toggleDiv() {
+  var x = document.getElementById("slide-menu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}

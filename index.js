b2653cf99aa8ff0f233346e1f00a2e3c437abd17
Matthew Rougely <matthewrougelysr@gmail.com>
2020-10-04 06:21:00 -0700

    circle animation

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function openNav() {
-  document.getElementById("mySidebar").style.clipPath = "circle(80% at 40% 50%)";
-  document.getElementById("mySidebar").style.width = "80%";
-  document.getElementById("box").style.marginLeft = "80%";
+  document.getElementById("mySidebar").style.clipPath =
+    "circle(80% at 40% 50%)";
+  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("box").style.marginLeft = "100%";
 }
-  
+
 function closeNav() {
+  document.getElementById("mySidebar").style.clipPath =
+    "circle(10% at 100% 0%)";
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("box").style.marginLeft= "0";
+  document.getElementById("box").style.marginLeft = "0";
 }
